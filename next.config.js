81129ac88ebb622ad53b330a1c07acb20fbb1433
aleksanderbastek <62114300+aleksanderbastek@users.noreply.github.com>
2021-08-13 17:05:17 +0200

    install antdesign and react-simple-map

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-	reactStrictMode: true
-};
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withLess = require("next-plugin-antd-less");
+const TsConfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
+module.exports = withLess({
+	env: {
+		// here add your environment variables
+	},
+	webpack(config) {
+		if (config.resolve.plugins) {
+			config.resolve.plugins.push(new TsConfigPathsPlugin());
+		} else {
+			config.resolve.plugins = [new TsConfigPathsPlugin()];
+		}
+
+		return config;
+	}
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
-
 const Home: NextPage = () => {
-	return <div></div>;
+	return <></>;
 };
 
 export default Home;
