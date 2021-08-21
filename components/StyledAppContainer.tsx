e5c44f98a31becbae60dad2f32641fea5eac8e78
aleksanderbastek <62114300+aleksanderbastek@users.noreply.github.com>
2021-08-21 15:38:47 +0200

    Create StyledAppContainer

---
+++ components/StyledAppContainer.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const SearchWrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	margin: 10px;
+`;
+export const ListWrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-end;
+	width: 40vw;
+`;
