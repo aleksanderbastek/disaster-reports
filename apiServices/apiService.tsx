60576275c4084ef1badb7dc7d499d8ea245e7416
aleksanderbastek <62114300+aleksanderbastek@users.noreply.github.com>
2021-08-21 15:36:14 +0200

    Create apiService

---
+++ apiServices/apiService.tsx
@@ -0,0 +1,22 @@
+import { useQuery } from "react-query";
+
+import { CountryInfoResponse, CountryInfo } from "../interfaces";
+
+export const useCountryDisasterNews = (country: string | null, page: number) => {
+	const filterValue = country === null ? "" : `&offset=${page}&filter[value]=${country}`;
+	const query = useQuery(["news_api", country, page], async () => {
+		if (filterValue !== "") {
+			const response = await fetch(
+				`https://api.reliefweb.int/v1/reports?appname=disaster-reports&filter[field]=disaster${filterValue}`
+			);
+
+			const json = (await response.json()) as CountryInfoResponse;
+
+			return json;
+		}
+
+		return null;
+	});
+
+	return query;
+};
