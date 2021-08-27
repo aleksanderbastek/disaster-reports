0fdd3a377762cf0ef0e1de5b550ca6d55a45e5d4
aleksanderbastek <62114300+aleksanderbastek@users.noreply.github.com>
2021-08-27 13:55:57 +0200

    Create MapChart Component

--- components/MapChart.tsx
+++ components/MapChart.tsx
@@ -1,68 +1,23 @@
-import { csv } from "d3-fetch";
 import { scaleLinear } from "d3-scale";
-import React, { useEffect, useState } from "react";
-import { useQuery } from "react-query";
+import React, { memo } from "react";
 import { ComposableMap, Geographies, Geography, Sphere, Graticule } from "react-simple-maps";
 
-interface CountryInfo {
-	fields: {
-		title: string;
-	};
-	href: string;
-	id: string;
-}
-
-interface CountryInfoResponse {
-	data: CountryInfo[];
-}
-
-const useCountryNews = (country: string | null) => {
-	const filterValue = country === null ? "" : `&filter[value]=${country}`;
-
-	const query = useQuery(["news_api", country], async () => {
-		if (filterValue !== null) {
-			const response = await fetch(
-				`https://api.reliefweb.int/v1/reports?appname=apidoc&filter[field]=disaster${filterValue}`
-			);
-
-			const json = (await response.json()) as CountryInfoResponse;
-
-			return json.data;
-		}
-
-		return null;
-	});
-
-	return query;
-};
+import { useCountryTotalCount } from "../apiServices/apiService";
 
 const geoUrl =
 	"https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
-const colorScale = scaleLinear().domain([0.29, 0.68]).range(["#ffedea", "#ff5233"]);
-
-const MapChart = () => {
-	const [data, setData] = useState([]);
-
-	const [selectedCountry, setSelectedCountry] = useState(null as string | null);
-	const countryQuery = useCountryNews(selectedCountry);
-
-	useEffect(() => {
-		csv(`/vulnerability.csv`).then(data => {
-			setData(data);
-		});
-	}, []);
-
-	const infoAboutCountry = countryQuery.isLoading ? (
-		<h2>Ładowanie</h2>
-	) : (
-		countryQuery.data.map(info => <div key={info.id}>{info.fields.title}</div>)
-	);
+const colorScale = scaleLinear().domain([1, 150]).range(["#ffedea", "#ff5233"]);
+interface Props {
+	selectCountry(country: string);
+	setTooltipContent(content: string);
+}
+const MapChart = ({ selectCountry, setTooltipContent }: Props) => {
+	const totalCountry = useCountryTotalCount();
 
 	return (
 		<>
-			<h1>{selectedCountry}</h1>
-			{infoAboutCountry}
 			<ComposableMap
+				data-tip=""
 				projectionConfig={{
 					rotate: [-10, 0, 0],
 					scale: 147
@@ -70,26 +25,53 @@ const MapChart = () => {
 			>
 				<Sphere stroke="#E4E5E6" strokeWidth={0.5} id="1" fill="#ffffff" />
 				<Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-				{data.length > 0 && (
-					<Geographies geography={geoUrl}>
-						{info =>
-							info.geographies.map(geo => {
-								const d = data.find(s => s.ISO3 === geo.properties.ISO_A3);
-								return (
-									<Geography
-										key={geo.rsmKey}
-										geography={geo}
-										onClick={() => setSelectedCountry(geo.properties.NAME)}
-										fill={"#F5F4F6"}
-									/>
-								);
-							})
-						}
-					</Geographies>
-				)}
+				<Geographies geography={geoUrl}>
+					{info =>
+						info.geographies.map(geo => {
+							const d = totalCountry.data.find(
+								s =>
+									s.value === geo.properties.NAME_LONG ||
+									s.value === geo.properties.NAME ||
+									s.value === geo.properties.FORMAL_EN ||
+									s.value === "Bolivia"
+							);
+							return (
+								<Geography
+									key={geo.rsmKey}
+									geography={geo}
+									onClick={() => selectCountry(geo.properties.NAME)}
+									fill={d ? colorScale(d.count) : "#F5F4F6"}
+									onMouseEnter={() => {
+										d
+											? setTooltipContent(
+													`${d.value} total cases - ${d.count}`
+											  )
+											: setTooltipContent("");
+									}}
+									onMouseLeave={() => {
+										setTooltipContent("");
+									}}
+									style={{
+										default: {
+											outline: "none"
+										},
+										pressed: {
+											fill: "#800080",
+											outline: "none"
+										},
+										hover: {
+											fill: "#800080",
+											outline: "none"
+										}
+									}}
+								/>
+							);
+						})
+					}
+				</Geographies>
 			</ComposableMap>
 		</>
 	);
 };
 
-export default MapChart;
+export default memo(MapChart);
