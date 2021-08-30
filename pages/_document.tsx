04c0a0979882ef08d1b5f8d76d392beddf0e20ab
aleksanderbastek <62114300+aleksanderbastek@users.noreply.github.com>
2021-08-30 16:29:06 +0200

    Add assets

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -62,6 +62,12 @@ export default class MyDocument extends Document<MyDocumentProps> {
 						href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@700&display=swap"
 						rel="stylesheet"
 					/>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Anton&display=swap"
+						rel="stylesheet"
+					/>
 					{this.props.styles}
 				</Head>
 				<body>
